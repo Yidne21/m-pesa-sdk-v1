263cb85e7bf80817d14f9f2df419401364779aa6
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2024-12-31 14:05:12 +0300

    Fix authConfigSchema consumerSecret type and update test to include environment

--- tests/auth/authService.test.ts
+++ tests/auth/authService.test.ts
@@ -6,7 +6,8 @@ describe("Auth Service", () => {
   test("Should return a token", async () => {
     const auth = new Auth(
       process.env.CONSUMER_KEY!,
-      process.env.CONSUMER_SECRET!
+      process.env.CONSUMER_SECRET!,
+      "sandbox" // or "production" based on your environment
     );
     const token = await auth.getToken();
     expect(token).toBeDefined();
